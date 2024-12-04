08ecf000aaf1eaf8d23adee4004a41d5f8a6dfa4
MR NOOB HERE ! <novana.xyz@gmail.com>
2024-12-05 01:57:48 +0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,5 +4,5 @@
 # VNC সার্ভার শুরু করুন
 su rdpuser -c 'vncserver :1 -geometry 1280x800 -depth 24'
 
-# noVNC শুরু করুন
+# noVNC শুরু করু
 /opt/novnc/utils/novnc_proxy --vnc localhost:5901 --listen 6080
