4a41ba12da185691668b2a6d95e9698fe7234a20
MR NOOB HERE ! <novana.xyz@gmail.com>
2024-12-05 03:04:26 +0600

    Create ngrok_method.sh

---
+++ ngrok_method.sh
@@ -0,0 +1,16 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y xrdp
+sudo apt install -y xfce4 xfce4-goodies
+sudo apt install -y tightvncserver
+sudo apt install -y dbus-x11 x11-xserver-utils
+echo "xfce4-session" > ~/.xsession
+sudo systemctl enable xrdp
+sudo systemctl start xrdp
+wget https://bin.equinox.io/c/4VmDzA7iaJ6/ngrok-stable-linux-amd64.zip
+unzip ngrok-stable-linux-amd64.zip
+sudo mv ngrok /usr/local/bin
+
+ngrok authtoken 
+
+ngrok tcp 3389
